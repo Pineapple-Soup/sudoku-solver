8ff469d0a02e8f57ab9f161da5c50aa46c2adb46
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-08-04 18:52:03 -0700

    feat: get puzzle data from CLI

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import sys
 
-def main(): ...
+def get_input():
+    try:
+        input = sys.argv[1]
+        if len(input) != 81:
+            raise SyntaxError
+        if not input.isdigit():
+            raise ValueError
+        return input
+    except IndexError:
+        print("Usage: solve.py PUZZLE_DATA")
+        exit(1)
+    except SyntaxError:
+        print("Invalid PUZZLE_DATA. Must be 81 Characters in length")
+        exit(1)
+    except ValueError:
+        print("Invalid PUZZLE_DATA. Puzzle can only contain digits 1-9")
+        exit(1)
+
+def main():
+    input = get_input()
+    print(input)
 
 
 if __name__ == "__main__":
