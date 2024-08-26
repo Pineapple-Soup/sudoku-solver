3ada73609e2f0dceee2e775ab07e75826ab1acc7
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-08-26 15:12:03 -0700

    build: parse user input into usable format

--- main.py
+++ main.py
@@ -18,9 +18,15 @@ def get_input() -> str:
         print("Invalid PUZZLE_DATA. Puzzle can only contain digits 1-9")
         exit(1)
 
+def parse_input(puzzle_data: str) -> list[list[str]]:
+    puzzle = [list(puzzle_data[i:i+9]) for i in range(0, len(puzzle_data), 9)]
+    return puzzle
+
 def main() -> None:
     input = get_input()
     print(input)
+    puzzle = parse_input(input)
+    print(puzzle)
 
 
 if __name__ == "__main__":
