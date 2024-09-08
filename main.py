dd43239d36f13b03854659611ea89e959cd53039
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-09-07 19:31:45 -0700

    build: check if a number is safe

--- main.py
+++ main.py
@@ -22,11 +22,29 @@ def parse_input(puzzle_data: str) -> list[list[str]]:
     puzzle = [list(puzzle_data[i:i+9]) for i in range(0, len(puzzle_data), 9)]
     return puzzle
 
+def find_next_empty_cell(puzzle: list[list[str]]) -> list[int]:
+    for r in range(0, len(puzzle)):
+        for c in range(0, len(puzzle[r])):
+            if puzzle[r][c] == '0':
+                return [r,c]
+    return [-1, -1]
+
+def check_square(puzzle: list[list[str]], r: int, c: int, num: int) -> bool:
+    if puzzle[r].count(num):
+        return False
+    if [row[c] for row in puzzle].count(num):
+        return False
+    for i in range(3):
+        for j in range(3):
+            if puzzle[i+((r//3)*3)][j+((c//3)*3)] == num:
+                return False
+    return True
+
 def main() -> None:
     input = get_input()
     print(input)
     puzzle = parse_input(input)
-    print(puzzle)
+    print(find_next_empty_cell(puzzle))
 
 
 if __name__ == "__main__":
