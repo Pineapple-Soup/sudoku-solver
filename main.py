ffaccf9f0036d28fcc579745cdcff5997697fa5e
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2024-12-15 01:26:53 -0800

    build: optimize grid building

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ def get_gui_input():
     window.geometry("600x600")
     window.resizable(width=False, height=False)
     window.configure(padx=10, pady=10, background='#ffc300')
+    for i in range(12):
+        window.grid_rowconfigure(i, weight=1)
+        window.grid_columnconfigure(i, weight=1)
 
     entries = []
     for row in range(9):
-        window.grid_rowconfigure(row, weight=1)
         puzzle_row = []
         for col in range(9):
-            window.grid_columnconfigure(col, weight=1)
             entry = tk.Entry(window, bg='#ffc86c', fg='black', highlightthickness='0', font=('Arial', 18), justify='center', validate="key", vcmd=(window.register(validate_input), '%P'))
             entry.grid(row=row, column=col, sticky='NSEW')
             entry.insert(0, tk.END)
@@ -33,9 +34,7 @@ def get_gui_input():
     clear_button = tk.Button(window, text="Clear", font=('Arial', 18), width=6, command=lambda: [[entry.delete(0, tk.END) for entry in row] for row in entries])
     solve_button = tk.Button(window, text="Invalid Puzzle", font=('Arial', 18), width=6, state=tk.DISABLED, command=lambda: handle_puzzle([[entry.get() or "0" for entry in row] for row in entries], entries=entries))
     quit_button = tk.Button(window, text="Quit", font=('Arial', 18), width=6, command=window.destroy)
-
-    for i in range(9, 12):
-        window.grid_rowconfigure(i, weight=1)
+    
     clear_button.grid(row=10, column=0, columnspan=3, sticky='NSEW')
     solve_button.grid(row=10, column=3, columnspan=3, sticky='NSEW')
     quit_button.grid(row=10, column=6, columnspan=3, sticky='NSEW')
